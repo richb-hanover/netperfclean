6c758e7d83e12c824b353818a05fad77004d0fd3
Rich Brown <richb.hanover@gmail.com>
2016-06-30 10:13:48 -0400

    Update .gitignore; kernlogcat.sh now filters properly

--- kernlogcat.sh
+++ kernlogcat.sh
@@ -3,11 +3,27 @@
 # Concatenate all the kern.log files from last week into a single file
 #
 
-zcat /var/log/kern.log.7.gz > ~/kernlog.txt
-zcat /var/log/kern.log.6.gz >> ~/kernlog.txt
-zcat /var/log/kern.log.5.gz >> ~/kernlog.txt
-zcat /var/log/kern.log.4.gz >> ~/kernlog.txt
-zcat /var/log/kern.log.3.gz >> ~/kernlog.txt
-zcat /var/log/kern.log.2.gz >> ~/kernlog.txt
-cat /var/log/kern.log.1 >> ~/kernlog.txt
-cat /var/log/kern.log >> ~/kernlog.txt
+# Scan the log file for specified entries
+scan_for() {
+    cat $INFILE | \
+    grep $1 | \
+    grep "IN=venet0" | \
+    sed -E 's/^.*SRC=//g' | \
+    sed -E 's/DST=.*$//g' | \
+    sort | uniq -c | sort -nr
+}
+
+zcat /var/log/kern.log.7.gz >  kernlog.txt
+zcat /var/log/kern.log.6.gz >> kernlog.txt
+zcat /var/log/kern.log.5.gz >> kernlog.txt
+zcat /var/log/kern.log.4.gz >> kernlog.txt
+zcat /var/log/kern.log.3.gz >> kernlog.txt
+zcat /var/log/kern.log.2.gz >> kernlog.txt
+cat /var/log/kern.log.1     >> kernlog.txt
+cat /var/log/kern.log       >> kernlog.txt
+
+INFILE='kernlog.txt'
+scan_for "Incoming" | \
+cat > junk.txt
+rm kernlog.txt
+
