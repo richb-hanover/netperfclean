84e5dccf696ebf4f4c4dee6083f1345224941e1b
Rich Brown <richb.hanover@gmail.com>
2016-09-08 09:28:31 -0400

    Update description of checkdistrib.sh; newest iptables.txt

--- checkdistrib.sh
+++ checkdistrib.sh
@@ -8,7 +8,14 @@
 # To use:
 # sudo sh findunfilteredips.sh # to create the updated kernlog.txt file
 # sh checkdistrib.sh <ip-address>
-
+#
+# Format of kernlog.txt
+#
+# Sep  1 03:10:57 netperf kernel: [3582360.450529] Dropped netperf  ...
+#
+# - First remove everything from 'netperf kernel' to the end of the line
+# - then remove the last six characters (the :mm:ss) leaving only the hours
+# - summarize the hours
 
 cat kernlog.txt | \
 grep "Incoming netperf" | \
